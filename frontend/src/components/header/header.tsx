9888cb6f97eee5a2d234936f97e0176ad1ab31a3
smartfellah <smartfellah@outlook.com>
2023-12-17 20:23:18 +0300

    feat: :sparkles: Add header template

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,13 @@
+import styles from "./header.module.css";
+
+import React, { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <div className={styles.Header}>
+      <div></div>
+    </div>
+  );
+};
+
+export { Header };
