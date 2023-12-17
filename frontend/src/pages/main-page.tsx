0b2ed87dca6ad1126406a69bc693a79c33e9a31d
smartfellah <smartfellah@outlook.com>
2023-12-18 00:56:10 +0300

    feat: :sparkles: Add main page

--- frontend/src/pages/main-page.tsx
+++ frontend/src/pages/main-page.tsx
@@ -1,7 +1,53 @@
-import { FC } from "react";
+import styles from "./main-page.module.css";
+
+import { createSearchParams, useNavigate } from "react-router-dom";
+
+import { FC, useState } from "react";
 
 const MainPage: FC = () => {
-  return <></>;
+  const navigate = useNavigate();
+  const [value, setValue] = useState<string>("");
+
+  function handleChange(event: React.FormEvent<HTMLInputElement>) {
+    setValue(event.currentTarget.value);
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    navigate({
+      pathname: "search",
+      search: `?${createSearchParams({
+        search: value,
+      })}`,
+    });
+  };
+
+  return (
+    <div className={styles.Container}>
+      <div></div>
+      <div className={styles.BannerContainer}>
+        <p className={styles.BannerText}>
+          Labor was the first price, the original purchase - money that was paid
+          for all things.
+        </p>
+      </div>
+      <form onSubmit={handleSubmit} className={styles.ButtonContainer}>
+        <input
+          onChange={handleChange}
+          className={styles.TextInput}
+          type="text"
+          name=""
+          id=""
+        />
+
+        <button type="submit" className={styles.SearchButton}>
+          <p className={styles.ButtonText}>Find the job</p>
+        </button>
+      </form>
+      <div></div>
+    </div>
+  );
 };
 
 export { MainPage };
