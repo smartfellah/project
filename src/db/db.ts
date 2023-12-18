b16f4af5f0c896fc0f44d9bbfc20200d487bc11a
smartfellah <smartfellah@outlook.com>
2023-12-19 01:10:34 +0300

    build: :building_construction: Add db-migrate module

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "vacancies",
+  database: "hh",
   password: "postgres",
   port: 5432,
 });
