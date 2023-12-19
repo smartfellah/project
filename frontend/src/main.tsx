58d6fd3fdbb872ccb028f195a722316f76dab126
smartfellah <smartfellah@outlook.com>
2023-12-19 19:23:39 +0300

    refactor: :recycle: Create store

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,8 +4,14 @@ import App from "./App.tsx";
 import "reset-css/reset.css";
 import "./index.css";
 
+//import redux
+import { Provider } from "react-redux";
+import { store } from "./services/store.ts";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
