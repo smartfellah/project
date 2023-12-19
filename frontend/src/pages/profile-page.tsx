e565eeec04fc329242ae99b40f49ed42d9ce33cf
smartfellah <smartfellah@outlook.com>
2023-12-19 21:00:24 +0300

    feat: :sparkles: Add some kind of design on profile page

--- frontend/src/pages/profile-page.tsx
+++ frontend/src/pages/profile-page.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 
+import styles from "./profile-page.module.css";
+
 import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../services/hooks";
 import { User, addUser, userSelector } from "../services/reducers/userSlice";
@@ -25,30 +27,63 @@ const ProfilePage: FC = () => {
     dispatch(addUser(newUser));
   }
   return (
-    <div>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.id} | {user.name} | {user.email}
-        </li>
-      ))}
-      <div>
-        <input
-          type="text"
-          placeholder="Name"
-          aria-label="name"
-          value={newUserName}
-          onChange={(e) => setNewUserName(e.target.value)}
-        ></input>
-        <input
-          type="text"
-          placeholder="Email"
-          aria-label="email"
-          value={newUserEmail}
-          onChange={(e) => setNewUserEmail(e.target.value)}
-        ></input>
-        <button type="submit" className="btn" onClick={handleAddUser}>
-          Add
-        </button>
+    <div className={styles.Container}>
+      <div className={styles.OptionsContainer}>
+        <p className={styles.MenuText}>Profile settings</p>
+        <div className={styles.Options}>
+          {/* {users.map((user) => (
+            <li key={user.id}>
+              {user.id} | {user.name} | {user.email}
+            </li>
+          ))} */}
+          <div className={styles.InfoBar}>
+            <div className={styles.InfoBox1}>
+              <p className={styles.InfoText}>Name</p>
+            </div>
+            <div className={styles.InfoBox2}>
+              <p className={styles.InfoText}>John</p>
+              <p className={styles.InfoText}>Изменить</p>
+            </div>
+          </div>
+          <div className={styles.InfoBar}>
+            <div className={styles.InfoBox1}>
+              <p className={styles.InfoText}>Name</p>
+            </div>
+            <div className={styles.InfoBox2}>
+              <p className={styles.InfoText}>John</p>
+              <p className={styles.InfoText}>Изменить</p>
+            </div>
+          </div>
+          <div className={styles.InfoBar}>
+            <div className={styles.InfoBox1}>
+              <p className={styles.InfoText}>Name</p>
+            </div>
+            <div className={styles.InfoBox2}>
+              <p className={styles.InfoText}>John</p>
+              <p className={styles.InfoText}>Изменить</p>
+            </div>
+          </div>
+
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              aria-label="name"
+              value={newUserName}
+              onChange={(e) => setNewUserName(e.target.value)}
+            ></input>
+            <input
+              type="text"
+              placeholder="Email"
+              aria-label="email"
+              value={newUserEmail}
+              onChange={(e) => setNewUserEmail(e.target.value)}
+            ></input>
+            <button type="submit" className="btn" onClick={handleAddUser}>
+              Add
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
