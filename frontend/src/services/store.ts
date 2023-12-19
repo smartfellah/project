7b112c340ab1b63081efa31439dffa19ba7259e6
smartfellah <smartfellah@outlook.com>
2023-12-19 19:38:59 +0300

    feat: :sparkles: Add user reducer and page prototype

--- frontend/src/services/store.ts
+++ frontend/src/services/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./reducers/userSlice";
 
 export const store = configureStore({
   reducer: {
-    //
+    userReducer,
   },
 });
 
